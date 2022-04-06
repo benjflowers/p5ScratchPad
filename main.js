ffec7426c158f09b25fac69733d724744d11a7f7
benjflowers <benjaminjflowers@gmail.com>
2022-04-05 20:17:21 -0500

    got a grid but doing other stuff

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-const width = 2000;
-const height = 2000;
+const width = 300;
+const height = 300;
 
 let sketch = p5 => {
+  var scale;
+  var cols;
+  var rows;
   p5.setup = () => {
     p5.createCanvas(width, height);
     p5.background(84, 201, 255);
 
     p5.noLoop();
+
+    scale = .30;
+    console.log(`resolution: ${width/ (width * scale)} x ${height/ (height * scale)}`)
+    cols = Math.floor(width/ (width * scale));
+    rows = Math.floor(height / (height * scale));
   };
 
   p5.draw = () => {
-    p5.strokeWeight(30);
-    p5.point(width/2, height/2);
+    p5.strokeWeight(3);
+
+    for(let i = 1; i < cols; i ++) {
+      p5.line(i * (width/cols), 0, i * (width/cols), height);
+    }
+
+    for(let i = 1; i < rows; i ++) {
+      p5.line(0, i * (height/rows), height, i * (height/rows));
+    }
   };
 };
 
